d6007fbe40ab397089ee4c87e9cf369ad62ee7d7
fabri <fabritzioalejandrol@gmail.com>
2025-08-07 09:48:15 -0600

    Metodo de mostrr libros y funcion de agregar libro

--- actividad16.py
+++ actividad16.py
@@ -3,3 +3,14 @@ class Libro:
         self.titulo= titulo
         self.autor= autor
         self.año= año
+    def mostrar_libros(self):
+        print(f"Titulo: {self.titulo} | Autor: {self.autor} | Año de publicacion: {self.año}")
+def agregar_libro():
+    try:
+        titulo= input("Ingrese el titulo del libro: ")
+        autor= input("Ingrese el autor del libro: ")
+        año_publicacion= int(input("Ingrese el año de publicacion: "))
+    except ValueError:
+        print("Error: el año de publicacion debe ser un numero entero.")
+    except Exception as e:
+        print(f"Ocurrio un error inesperado: ",e)
